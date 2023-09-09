a284460b2a2bac5292a34d5eaffac844878a28b4
chinnamrajukasina <chinnamrajukaina@gmail.com>
2023-09-09 10:40:10 +0530

    initial commit

---
+++ task/getSet.js
@@ -0,0 +1,22 @@
+class Person{
+    constructor(name, city){
+        this.name = name,
+        this.city = city;    
+    }
+    set setName(name){
+        this.name = name
+    }
+    set setCity(city){
+        this.city = city
+    }
+    get getName(){
+        return this.name
+    }
+    get getCity(){
+        return this.city
+    }
+}
+person1 = new Person('tony', 'newYork');
+person2 = new Person();
+console.log(person1)
+console.log(person2)
