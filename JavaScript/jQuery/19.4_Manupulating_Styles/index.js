8bfb5c5a4e21ff591c77bb5e1fc00f9cf6770d40
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-10-18 10:11:48 +0530

    initial commit

---
+++ JavaScript/jQuery/19.4_Manupulating_Styles/index.js
@@ -0,0 +1,7 @@
+// $("h1").addClass("big-title");
+// $("h1").addClass("margin-50");
+//both can be merged
+$("h1").addClass("big-title margin-50");
+if($("h1").hasClass("margin-50")){
+    $("h1").addClass("red");
+}
