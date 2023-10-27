5f42934824861f10f841614132a70692110ae6df
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-10-27 08:13:28 +0530

    initial commint

---
+++ React/alien-clock/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import AlienClockApp from './components/app';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <AlienClockApp/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
