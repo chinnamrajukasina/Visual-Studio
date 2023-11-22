6aa2b10a5e10267fb53a58bc76af89429d20e622
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-11-22 18:21:07 +0530

    updated code

--- task/codeathon/HomePage/index.js
+++ task/codeathon/HomePage/index.js
@@ -16,8 +16,6 @@ function toggleView(view) {
     }
 }
 
-
-
 function showInfo(infoNumber) {
     // Hide all info elements
     var infoElements = document.querySelectorAll('.info');
@@ -53,7 +51,6 @@ var gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach(function (element) {
     element.addEventListener('click', handleGridItemClick);
 });
-// index.js
 
 // Other functions...
 function toggleContactDropdown() {
@@ -64,8 +61,32 @@ function toggleContactDropdown() {
         dropdownContent.style.display = 'block';
     }
 }
-   // JavaScript function to open the new page
-   function openNewPage() {
+
+// JavaScript function to open the new page
+function openNewPage() {
     // Redirect to the desired HTML page
-    window.location.href = '../testFile/index.html' // Replace with the actual page path
+    window.location.href = '../testFile/index.html'; // Replace with the actual page path
 }
+// Variable for switch instance
+var switchElement = document.getElementById('mySwitch');
+
+// Add an event listener to the switch
+switchElement.addEventListener('change', function () {
+    // Toggle the background color of the body
+    document.body.style.backgroundColor = switchElement.checked ? 'black' : 'white';
+
+    // Toggle the color of the h1 element
+    document.querySelector('h1').style.color = switchElement.checked ? 'white' : 'black';
+
+    // Toggle the color of all elements with the class 'info' and 'active'
+    var infoElements = document.querySelectorAll('.info.active');
+    infoElements.forEach(function (element) {
+        // Check if the element has a style property before setting color
+        if (element.style) {
+            element.style.color = switchElement.checked ? 'blue' : 'pink';
+        }
+    });
+});
+
+// Trigger the change event to handle the initial state
+switchElement.dispatchEvent(new Event('change'));
--- task/codeathon/testFile/index.js
+++ task/codeathon/testFile/index.js
@@ -6,3 +6,11 @@ document.getElementById('myForm').addEventListener('submit', function (event) {
     // Handle form submission logic here
     console.log('Form submitted!');
 });
+
+var switchElement = document.getElementById('mySwitch');
+
+// Add an event listener to the switch
+switchElement.addEventListener('change', function () {
+  // Toggle the background color of the body
+  document.body.style.backgroundColor = switchElement.checked ? 'tan' : '#f4f4f4';
+});
