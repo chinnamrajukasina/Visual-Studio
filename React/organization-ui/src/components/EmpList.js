574b855b33c1750c5a12049b2a9e81ed6aaecc38
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-12-02 08:20:38 +0530

    cleaned OrgUI

--- React/organization-ui/src/components/EmpList.js
+++ React/organization-ui/src/components/EmpList.js
@@ -3,20 +3,11 @@ import Emp from './Emp';
 
 
 export default function EmpList(data) {
-  // const [emps, setEmps] = useState(data.emps);
-  const emps=data.emps
-
+  const emps = data.emps
   const handleUpdateEmployee = data.handleUpdateEmployee;
   const handleRemoveEmp = data.handleRemoveEmp
-  // const handleRemoveEmp = (id) => {
-  //   console.log("Remove operation on ", id);
-  //   var updatedEmps = emps.filter(emp => emp.id !== id);
-  //   setEmps(updatedEmps)
-  // }
-
-
+  
   return (
-
     emps && emps.map(emp => <Emp
       emp={emp}
       key={emp.id}
--- React/organization-ui/src/components/OrgList.js
+++ React/organization-ui/src/components/OrgList.js
@@ -21,36 +21,20 @@ export default function OrgList({ orgs: initialOrgs }) {
 
   const handleUpdateOrg = async (updatedOrg) => {
     console.log("Update operation on ", updatedOrg.id);
-  
+
     // Make the update in the frontend
     setOrgs((prevOrgs) =>
       prevOrgs.map((org) =>
         org.id === updatedOrg.id ? { ...org, name: updatedOrg.name, address: updatedOrg.address } : org
       )
     );
-  
-    var updateResult = await updateOrg(updatedOrg);
-    console.log('org updated in backend' ,updateResult)
-  };
-  
 
-  // const handleAddEmployee = (orgId) => {
-  //   setOrgs(prevOrgs =>
-  //     prevOrgs.map(org =>
-  //       org._id === orgId ? {
-  //         ...org,
-  //         employees: [
-  //           ...org.employees,
-  //           {
-  //             id: org.employees.length + 1,
-  //             name: `Lecturar${org.employees.length + 1}`,
-  //             email: `Lecturar${org.employees.length + 1}@aec.edu.in`
-  //           }
-  //         ]
-  //       } : org
-  //     )
-  //   );
-  // };
+    var updateResult = await updateOrg(updatedOrg);
+    console.log('org updated in backend', updateResult)
+  };
+
+
+
 
 
   return (
@@ -60,9 +44,7 @@ export default function OrgList({ orgs: initialOrgs }) {
           key={org.id}
           currentOrg={org}
           handleRemoveOrg={handleRemoveOrg}
-          handleUpdateOrg={handleUpdateOrg}
-          // handleAddEmployee={() => handleAddEmployee(org.id)}
-        />
+          handleUpdateOrg={handleUpdateOrg} />
       )}
     </div>
   );
