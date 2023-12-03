8c94f84deda8eff88782917af87dc1f41487b4b3
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-12-03 11:17:49 +0530

    updated

--- React/react-practice/src/SideMenu.jsx
+++ React/react-practice/src/SideMenu.jsx
@@ -4,6 +4,8 @@ import Hello from './components/Hello';
 import AppRouter from './components/Router/AppRouter';
 import Counter from './components/CustomCounter/Counter';
 import CounterReducer from './components/CustomCounter/CounterReducer';
+import PropsContact from './components/ReactProps/components/PropsContact';
+
 
 
 const SideMenu = ({ onSelectComponent }) => {
@@ -13,6 +15,7 @@ const SideMenu = ({ onSelectComponent }) => {
       <div className="menu-item" onClick={() => onSelectComponent(<AppRouter />)}> AppRouter</div>  
       <div className="menu-item" onClick={() => onSelectComponent(<Counter />)}> Counter</div>
       <div className="menu-item" onClick={() => onSelectComponent(<CounterReducer />)}> CounterReducer</div>
+      <div className="menu-item" onClick={() => onSelectComponent(<PropsContact />)}> PropsContact</div>
     </div>
   );
 };
---
+++ React/react-practice/src/components/ReactProps/components/PropsContact.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+ import '../styles.css';
+
+function createCard(contact) {
+  return (
+    <Card class ='PropsContact'
+      id ={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+function PropsContact() {
+  return (
+    <div>
+      <h1 className="PropsContactHeading">My Contacts</h1>
+      {contacts.map(createCard)}
+    </div>
+  );
+}
+
+export default PropsContact;
