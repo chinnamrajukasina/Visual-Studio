a08605d9dca07b63b55feb9a7f2810f55eb15355
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-12-03 19:30:43 +0530

    betting game

--- React/react-practice/src/components/BettingGame/BettingGame.js
+++ React/react-practice/src/components/BettingGame/BettingGame.js
@@ -1,17 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function BettingGame() {
+  const [result, setResult] = useState(null);
+  const [p1, setP1] = useState(null);
+  const [p2, setP2] = useState(null);
+
+  const startGame = () => {
+    const newP1 = Math.floor(Math.random() * 6) + 1;
+    const newP2 = Math.floor(Math.random() * 6) + 1;
+    let gameResult = newP1 > newP2 ? 'Player 1 Wins!' : newP1 < newP2 ? 'Player 2 Wins!' : 'Draw!';
+    setP1(newP1);
+    setP2(newP2);
+    setResult(gameResult);
+  };
+
   return (
     <div>
-    <header><h1>Welcome to the Game</h1></header>
-    <div>
-    <h1>Plater 1</h1>
+      <header style={{ textAlign: 'center', marginBottom: '20px', border: '1px solid #ccc'}}>
+        <h1>Welcome to the Game</h1>
+        <div style={{ border: '1px solid #ccc', padding: '10px'  }}>
+          <h2 >{result}</h2>
+        </div>
+      </header>
+
+      <div style={{ display: 'flex', justifyContent: 'space-around' , textAlign:'center'}}>
+        <div style={{ border: '1px solid #ccc', padding: '10px' }}>
+          <h1>{p1}</h1>
+          <h2>Player 1</h2>
+        </div>
+        <div style={{ border: '1px solid #ccc', padding: '10px' }}>
+          <h1>{p2}</h1>
+          <h2>Player 2</h2>
+        </div>
+      </div>
+
+      <button onClick={startGame} style={{ display: 'block', margin: '20px auto', padding: '10px' }}>
+        Start
+      </button>
     </div>
-    <div>
-    <h1>Plater 2</h1>
-    </div>
-    <button>Start</button>
-    
-    </div>
-  )
+  );
 }
