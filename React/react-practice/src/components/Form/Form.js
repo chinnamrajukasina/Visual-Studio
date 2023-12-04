40e90aafff655ec1be8843b4d7c1d634587a5bc1
chinnamrajukasina <chinnamrajukasina@gmail.com>
2023-12-04 13:22:50 +0530

    completed form task

--- React/react-practice/src/components/Form/Form.js
+++ React/react-practice/src/components/Form/Form.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function Form() {
+  const [name, setName] = useState('');
+  const [newName, setNewName] = useState('')
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+  function handleClick(event){
+    setNewName(name);
+    setName('');
+    event.preventDefault()
+    
+  }
+
   return (
-    <div >
-      <h1>Hello</h1>
-      <input type='text'></input>
+    <div style={{ padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <form onSubmit={handleClick}>
+      <h1>Hello {newName}</h1>
+      <input
+        type='text'
+        value={name}
+        placeholder='Username'
+        onChange={handleChange}
+        style={{ padding: '10px', marginRight: '10px' }}
+      />
+      <button style={{ margin: '10px', scale: '1.5' }} >
+        Submit
+      </button></form>
     </div>
-  )
+  );
 }
