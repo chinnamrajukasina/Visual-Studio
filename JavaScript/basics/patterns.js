b1a53b8602992af784bdf1339797813ea326cadc
chinnamrajukasina <chinnamrajukasina@gmail.com>
2024-01-18 11:38:51 +0530

    right-angled triangle

--- JavaScript/basics/patterns.js
+++ JavaScript/basics/patterns.js
@@ -1,2 +1,10 @@
-var n=5;
+var n = 5;
+console.log(n);
 
+for (var i = 1; i <= n; i++) {
+    var row = "";
+    for (var j = 1; j <= i; j++) {
+        row += "* ";
+    }
+    console.log(row);
+}
